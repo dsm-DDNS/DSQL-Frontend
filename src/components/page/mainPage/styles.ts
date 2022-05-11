4f6e37c14492c3e4f58e0546807b5bca941252b1
eastcopper <ldh7228@gmail.com>
2022-05-11 15:41:45 +0900

    ⚡ :: (#28)[MainPage] Design MainPage

--- src/components/page/mainPage/styles.ts
+++ src/components/page/mainPage/styles.ts
@@ -26,3 +26,43 @@ export const Explain = styled.div`
   font-size: 30px;
 `;
 
+export const ProjectList = styled.div`
+  width: 100%;
+  box-sizing: border-box;
+  padding: 0px 127px;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-auto-rows: minmax(100px, auto);
+  grid-row-gap: 10px;
+  grid-column-gap: 10px;
+`;
+
+export const Footer = styled.div`
+  margin: 65px 0px;
+  width: 100%;
+  box-sizing: border-box;
+  padding: 0px 167px;
+  display: inline-flex;
+  justify-content: space-between;
+`;
+
+export const Developer = styled.div`
+  position: relative;
+  width: 276px;
+  height: 98px;
+  hr {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 200px;
+    height: 10px;
+    border: none;
+    background-color: #a582c2;
+  }
+  div {
+    position: absolute;
+    bottom: 0;
+    right: 0;
+    font-size: 70px;
+  }
+`;
