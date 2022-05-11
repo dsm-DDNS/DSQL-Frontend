85b6194f3577bc7a6b2b60495850c90534887bba
eastcopper <ldh7228@gmail.com>
2022-05-11 13:58:26 +0900

    ⚡ :: (#09)[MainPage] Design MainPage

---
+++ src/components/page/mainPage/styles.ts
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+export const MainDiv = styled.div`
+  width: 100%;
+`;
+
+export const Banner = styled.div`
+  width: 100%;
+  height: 1000px;
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+`;
+
+export const ExplainContainer = styled.div`
+  width: 100%;
+  box-sizing: border-box;
+  padding: 0px 100px 0px 126px;
+`;
+
+export const Explain = styled.div`
+  display: flex;
+  color: #c9c9c9;
+  font-size: 30px;
+`;
+
